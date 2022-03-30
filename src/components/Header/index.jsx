b637d809bba25b869d43eb26fb7fc9f863a73f89
Vinicius-SO <vhisoares39@gmail.com>
2022-03-30 11:39:08 -0300

    Header Function component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,11 +4,10 @@ import { FiPlusSquare } from 'react-icons/fi';
 import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
 
-class Header extends Component {
-  render() {
-    const { openModal } = this.props;
 
-    return (
+export const Header = ({openModal}) => {
+  
+  return(
       <Container>
         <header>
           <img src={Logo} alt="GoRestaurant" />
@@ -27,8 +26,5 @@ class Header extends Component {
           </nav>
         </header>
       </Container>
-    )
-  }
-};
-
-export default Header;
+  )
+}
